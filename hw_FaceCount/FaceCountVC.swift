66fecc17f598d90c586fa24fd6d073648dee9227
mavlonergashev <mavlik.exe@gmail.com>
2022-03-29 19:11:38 +0500

    hw fully completed

--- hw_FaceCount/FaceCountVC.swift
+++ hw_FaceCount/FaceCountVC.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class FaceCountVC: UIViewController {
 
@@ -22,6 +24,19 @@ class FaceCountVC: UIViewController {
     
     @IBAction func detectFacesBtnPressed(_ sender: Any) {
         
+        self.sendImageAndGetFacesCount(img: imageView.image!) { count in
+            
+            self.resultStackView.isHidden = false
+            self.resultLbl.text = "Detected \(count!) faces on photo"
+            self.resultLbl.textColor = .black
+            
+            if count == "0" {
+                self.resultLbl.text = "No face was detected on photo"
+                self.resultLbl.textColor = .red
+            }
+            
+        }
+        
     }
     
     @IBAction func choosePhotoBtnPressed(_ sender: Any) {
@@ -32,6 +47,44 @@ class FaceCountVC: UIViewController {
         vc.allowsEditing = true
         vc.delegate = self
         present(vc, animated: true)
+    }
+    
+    func sendImageAndGetFacesCount(img: UIImage, done: @escaping (String?) -> Void) {
+        
+        let username = "acc_7c313d0c50e0078"
+        let password = "f8d426959d4f08f419e05f2e44cb9e59"
+        let loginString = String(format: "%@:%@", username, password)
+        let loginData = loginString.data(using: String.Encoding.utf8)!
+        let base64LoginString = loginData.base64EncodedString()
+        
+        var request = URLRequest(url: URL(string: "https://api.imagga.com/v2/faces/detections")!)
+        
+        request.httpMethod = "POST"
+        request.addValue("Basic \(base64LoginString)", forHTTPHeaderField: "Authorization")
+        
+        AF.upload(multipartFormData: { multipart in
+            
+            let imgData = img.pngData()!
+            
+            multipart.append(imgData, withName: "image", fileName: "img.png", mimeType: "image/png")
+            
+        }, with: request).response { response in
+            if let data = response.data {
+                
+                print("Succesful Response")
+                
+                let jsonData = JSON(data)
+                
+                let count = "\(jsonData["result"]["faces"].arrayValue.count)"
+                
+                done(count)
+                
+            } else {
+                done(nil)
+                print(response.error.debugDescription)
+            }
+        }
+        
     }
     
 }
