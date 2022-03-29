125a3a4911195ee53f0f3e4697663e16e765523b
mavlonergashev <mavlik.exe@gmail.com>
2022-03-29 12:46:53 +0500

    IBOutlets and IBActions setted

--- hw_FaceCount/FaceCountVC.swift
+++ hw_FaceCount/FaceCountVC.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class FaceCountVC: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var resultLbl: UILabel!
+    @IBOutlet weak var resultStackView: UIStackView!
+    @IBOutlet weak var chooseBtn: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        
     }
-
-
+    
+    @IBAction func detectFacesBtnPressed(_ sender: Any) {
+        
+    }
+    
+    @IBAction func choosePhotoBtnPressed(_ sender: Any) {
+        
+    }
+    
 }
--- hw_FaceCount/TextPhotoVC.swift
+++ hw_FaceCount/TextPhotoVC.swift
@@ -8,11 +8,24 @@
 import UIKit
 
 class TextPhotoVC: UIViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var resultLbl: UILabel!
+    @IBOutlet weak var resultStackView: UIStackView!
+    @IBOutlet weak var chooseBtn: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        
     }
-
+    
+    @IBAction func detectTextBtnPressed(_ sender: Any) {
+        
+    }
+    
+    @IBAction func choosePhotoPressed(_ sender: Any) {
+        
+    }
+    
 }
